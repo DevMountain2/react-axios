80aabd262c9bc13abf3eca40dcc7d46801bf2d84
ZachGerhart <zgerhart@gmail.com>
2017-10-17 17:57:52 -0500

    modified

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Header from './Header/Header';
 import List from './List/List';
 import Workspace from './Workspace/Workspace';
 
+import { getCustomerList, createCustomer, getCustomer, updateCustomer, deleteCustomer } from "../customers";
+
 
 class App extends Component {
   constructor() {
@@ -15,7 +17,72 @@ class App extends Component {
       creating: false,
       currentCustomer: null
     }
+      this.startNewCustomer = this.startNewCustomer.bind(this);
+      this.createCustomer = this.createCustomer.bind(this);
+      this.selectCustomer = this.selectCustomer.bind(this);
+      this.saveEdit = this.saveEdit.bind(this)
+      this.removeCustomer = this.removeCustomer.bind(this)
 
+  }
+
+  removeCustomer(id) {
+  deleteCustomer(id).then(deletedCustomer =>{
+    getCustomerList().then(list =>{
+      this.setState({
+        customerList: list,
+        currentCustomer: null,
+        initialLoad: true
+      })
+    })
+  })
+}
+
+  saveEdit(id, obj) {
+    updateCustomer(id, obj).then(updatedCustomer => {
+      getCustomerList().then(list => {
+        this.setState({
+          customerList: list,
+          currentCustomer: updatedCustomer
+        })
+      })
+    })
+  }
+
+  selectCustomer(id) {
+    getCustomer(id).then(response => {
+      this.setState ({
+        currentCustomer: response,
+        initialLoad: false
+      })
+    })
+  }
+
+
+  createCustomer(customer) {
+      createCustomer(customer).then(response => {
+        getCustomerList().then(list => {
+          this.setState({
+            initialLoad:true,
+            creating: false,
+            customerList: list
+          })
+        })
+      })
+    }
+
+
+  startNewCustomer() {
+    this.setState({
+      creating: true,
+      initialLoad: false,
+      currentCustomer: null
+    })
+  }
+
+  componentDidMount() {
+    getCustomerList().then(list => {
+      this.setState({ customerList: list });
+    })
   }
 
   render() {
@@ -27,12 +94,17 @@ class App extends Component {
             this.state.customerList ?
             <List
               customerList={this.state.customerList || []}
+              startNewCustomer = {this.startNewCustomer}
+              selectCustomer = {this.selectCustomer}
               />
             : null
           }
           <Workspace initialLoad={this.state.initialLoad}
+                    createCustomer = {this.createCustomer}
                     currentCustomer={this.state.currentCustomer}
                     creating={this.state.creating}
+                    saveEdit={this.saveEdit}
+                    removeCustomer={this.removeCustomer}
                   />
         </div>
       </div>
